a7c8fa2ce4d2223f6f3a176a66f8c13fe6eb8495
anand <anand@unilend.finance>
2021-07-12 18:38:03 +0530

    connect wallet and select chain modal UI

--- src/component/UI/modal/connectWalletModal.tsx
+++ src/component/UI/modal/connectWalletModal.tsx
@@ -1,11 +1,24 @@
 import React, { useContext } from "react";
+import { ThemeContext } from "context/themeContext";
+import MetamaskLogo from 'assets/metamask.svg';
+import { Modal, Button } from "react-bootstrap";
+import "./index.scss";
+import '../../../theme.scss';
 
 const ConnectWalletModal = (props) => {
+  const data: any = useContext(ThemeContext);
+   
   return (
-  <>
-  Connect Metamask
-  </>
-  
+    <div className={`App ${data.dark}`} >
+      <Modal  {...props} centered size='md' >
+        <Modal.Body className="custom-modalBody">
+        <Button className="custom-modal-btn" ><img src={MetamaskLogo} className="metamask-logo"/>Metamask</Button>
+
+        <Button className="custom-modal-btn" >WalletConnect</Button>
+
+        </Modal.Body>
+      </Modal>
+   </div>
   );
 };
 export default ConnectWalletModal;
