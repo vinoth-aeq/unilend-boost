b08beb4d33be4f7ad2d65adfa10e0eb4a749d645
Thanush Praveen <thanushpraveen@aequalisys.com>
2021-07-12 19:13:02 +0530

    Modal Theming Implementation

--- src/component/UI/modal/connectWalletModal.tsx
+++ src/component/UI/modal/connectWalletModal.tsx
@@ -1,24 +1,31 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "context/themeContext";
-import MetamaskLogo from 'assets/metamask.svg';
+import MetamaskLogo from "assets/metamask.svg";
 import { Modal, Button } from "react-bootstrap";
 import "./index.scss";
-import '../../../theme.scss';
+import "../../../theme.scss";
 
 const ConnectWalletModal = (props) => {
   const data: any = useContext(ThemeContext);
-   
+
   return (
-    <div className={`${data.theme}`} >
-      <Modal  {...props} centered size='md' >
+    <div>
+      <Modal
+        className={`model-switch-${data.theme}`}
+        {...props}
+        centered
+        size="md"
+      >
         <Modal.Body className="custom-modalBody">
-        <Button className="custom-modal-btn" ><img src={MetamaskLogo} className="metamask-logo"/>Metamask</Button>
+          <Button className="custom-modal-btn">
+            <img src={MetamaskLogo} className="metamask-logo" />
+            Metamask
+          </Button>
 
-        <Button className="custom-modal-btn" >WalletConnect</Button>
-
+          <Button className="custom-modal-btn">WalletConnect</Button>
         </Modal.Body>
       </Modal>
-   </div>
+    </div>
   );
 };
 export default ConnectWalletModal;
--- src/component/UI/modal/selectChainModal.tsx
+++ src/component/UI/modal/selectChainModal.tsx
@@ -1,34 +1,36 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "context/themeContext";
 import EthereumLogo from "assets/ethereumLogo.svg";
-import BinanceLogo from 'assets/binance.svg';
-import PolygonLogic from 'assets/polygonmaticLogo.svg';
-import { Modal, Row,Col } from "react-bootstrap";
+import BinanceLogo from "assets/binance.svg";
+import PolygonLogic from "assets/polygonmaticLogo.svg";
+import { Modal, Row, Col } from "react-bootstrap";
 import "./index.scss";
 
 const SelectChainModal = (props) => {
   const data: any = useContext(ThemeContext);
 
   return (
-    <div className={`${data.theme}`}>
-      <Modal {...props} centered>
-        <Modal.Header style={{ borderBottom: "0" }} >
+    <div>
+      <Modal className={`model-switch-${data.theme}`} {...props} centered>
+        <Modal.Header style={{ borderBottom: "0" }}>
           <Modal.Title className="selectchain-title">Select Chain</Modal.Title>
           {/* <div >X</div> */}
         </Modal.Header>
         <Modal.Body className="custom-selectchain-body">
-            <div className="selectchain-list" > 
-                <img alt="Logo" src={BinanceLogo} className="network-logo" />
-                <span className="network-title">Binance</span>
-            </div>
+          <div className="selectchain-list">
+            <img alt="Logo" src={BinanceLogo} className="network-logo" />
+            <span className="network-title">Binance</span>
+          </div>
 
-            <div className="selectchain-list" > 
+          <div className="selectchain-list">
             <img alt="Logo" src={EthereumLogo} className="network-logo" />
-                <span className="network-title">Ethereum</span></div>
+            <span className="network-title">Ethereum</span>
+          </div>
 
-                <div className="selectchain-list" > 
+          <div className="selectchain-list">
             <img alt="Logo" src={PolygonLogic} className="network-logo" />
-                <span className="network-title">Ploygon</span></div>
+            <span className="network-title">Ploygon</span>
+          </div>
         </Modal.Body>
       </Modal>
     </div>
