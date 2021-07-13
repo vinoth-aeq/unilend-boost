d7dc6a15432bf72717e34767783020d2673af56a
Thanush Praveen <thanushpraveen@aequalisys.com>
2021-07-13 18:45:57 +0530

    Dex list alignment

--- src/component/UI/modal/selectDexModal.tsx
+++ src/component/UI/modal/selectDexModal.tsx
@@ -1,42 +1,44 @@
-import React, { useContext,useState } from "react";
+import React, { useContext, useState } from "react";
 import { ThemeContext } from "context/themeContext";
-import VectorImg from 'assets/Vector.svg'
-import { Modal, Row,Col } from "react-bootstrap";
-import options from './dexData';
-import { Typeahead } from 'react-bootstrap-typeahead';
-import 'react-bootstrap-typeahead/css/Typeahead.css';
-import './dexModel.scss';
+import VectorImg from "assets/Vector.svg";
+import { Modal, Row, Col } from "react-bootstrap";
+import options from "./dexData";
+import { Typeahead } from "react-bootstrap-typeahead";
+import "react-bootstrap-typeahead/css/Typeahead.css";
+import "./dexModel.scss";
 
 const SelectDexModal = (props) => {
   const data: any = useContext(ThemeContext);
   const [selected, setSelected] = useState([]);
 
-
   return (
     <div>
-      <Modal  className={`model-switch-${data.theme}`} {...props} centered >
-        <Modal.Header className="custom-coin-modal" style={{borderBottom:'0'}}>
-        <Typeahead
-          inputProps={{
-            className: 'custom-typehead',
-            // style: {background:'red',borderRadius:'20px'}
-          }}
-          placeholder="Search DEX"
-          onChange={setSelected}
-          // options={options}
-          options={['one', 'two']}
-          selected={selected}
-        />
+      <Modal className={`model-switch-${data.theme}`} {...props} centered>
+        <Modal.Header
+          className="custom-coin-modal"
+          style={{ borderBottom: "0" }}
+        >
+          <Typeahead
+            inputProps={{
+              className: "custom-typehead",
+            }}
+            placeholder="Search DEX"
+            onChange={setSelected}
+            options={["one", "two"]}
+            selected={selected}
+          />
         </Modal.Header>
-        <Modal.Body >
+        <Modal.Body>
           <Row className="custom-row">
-          <Col><img src={VectorImg}/>Zeroswap</Col>
+            <div className="list-col">
+              <img src={VectorImg} />
+              Zeroswap
+            </div>
+            <div className="list-col">
+              <img src={VectorImg} />
+              Zeroswap
+            </div>
           </Row>
-          {/* <div className="custom-row-border"></div> */}
-          <Row className="custom-row">
-          <Col><img src={VectorImg}/>Zeroswap</Col>
-          </Row>
-
         </Modal.Body>
       </Modal>
     </div>
