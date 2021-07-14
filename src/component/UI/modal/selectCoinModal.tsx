fd2ed14884fae45a33dd5108d1950ed8076fc2a4
anand <anand@unilend.finance>
2021-07-14 11:07:36 +0530

    SElect coin modal and theme changes

--- src/component/UI/modal/selectCoinModal.tsx
+++ src/component/UI/modal/selectCoinModal.tsx
@@ -1,7 +1,8 @@
 import React, { useContext } from "react";
 import { ThemeContext } from "context/themeContext";
-import { Modal } from "react-bootstrap";
-import "./dexModel.scss";
+import EthereumLogo from "assets/ethereumLogo.svg";
+import { Modal, FormControl } from "react-bootstrap";
+import "./coinModel.scss";
 
 const SelectCoinModal = (props) => {
   const data: any = useContext(ThemeContext);
@@ -15,10 +16,144 @@ const SelectCoinModal = (props) => {
         centered
       >
         <Modal.Header closeButton style={{ borderBottom: "0" }}>
-          <Modal.Title>Select Coin</Modal.Title>
+          <Modal.Title>
+            <FormControl
+              className="custom-coin-search"
+              type="text"
+              placeholder="Search by name or address"
+            />
+          </Modal.Title>
         </Modal.Header>
-        <Modal.Body className="modal">
-          <p>hello</p>
+        <Modal.Body className="custom-coin-body">
+          <div className="selectcoin-list">
+            <div>
+              <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">ETH</span>
+            </div>
+            <div>
+              <span>$20.59</span>
+            </div>
+          </div>
+
+          <div className="selectcoin-list">
+          <div>
+          <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">ETH</span>
+          </div>
+             <div><span>$20.59</span></div>
+            </div>
+
+            <div className="selectcoin-list">
+          <div>
+          <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">ETH</span>
+          </div>
+             <div><span>$20.59</span></div>
+            </div>
+
+            <div className="selectcoin-list">
+            <div>
+              <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">ETH</span>
+            </div>
+            <div>
+              <span>$20.59</span>
+            </div>
+          </div>
+
+          <div className="selectcoin-list">
+          <div>
+          <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">ETH</span>
+          </div>
+             <div><span>$20.59</span></div>
+            </div>
+
+            <div className="selectcoin-list">
+          <div>
+          <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">ETH</span>
+          </div>
+             <div><span>$20.59</span></div>
+            </div>
+
+            <div className="selectcoin-list">
+            <div>
+              <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">ETH</span>
+            </div>
+            <div>
+              <span>$20.59</span>
+            </div>
+          </div>
+
+          <div className="selectcoin-list">
+          <div>
+          <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">ETH</span>
+          </div>
+             <div><span>$20.59</span></div>
+            </div>
+
+            <div className="selectcoin-list">
+          <div>
+          <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">ETH</span>
+          </div>
+             <div><span>$20.59</span></div>
+            </div>
+
+            <div className="selectcoin-list">
+            <div>
+              <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">ETH</span>
+            </div>
+            <div>
+              <span>$20.59</span>
+            </div>
+          </div>
+
+          <div className="selectcoin-list">
+          <div>
+          <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">name</span>
+          </div>
+             <div><span>$20.59</span></div>
+            </div>
+
+            <div className="selectcoin-list">
+          <div>
+          <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">name</span>
+          </div>
+             <div><span>$20.59</span></div>
+            </div>
+
+            <div className="selectcoin-list">
+            <div>
+              <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">name</span>
+            </div>
+            <div>
+              <span>$20.59</span>
+            </div>
+          </div>
+
+          <div className="selectcoin-list">
+          <div>
+          <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">name</span>
+          </div>
+             <div><span>$20.59</span></div>
+            </div>
+
+            <div className="selectcoin-list">
+          <div>
+          <img alt="Logo" src={EthereumLogo} className="coin-logo" />
+              <span className="coin-title">name</span>
+          </div>
+             <div><span>$20.59</span></div>
+            </div>
         </Modal.Body>
       </Modal>
     </div>
